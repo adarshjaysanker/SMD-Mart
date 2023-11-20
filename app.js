21265bbd30144d8b10c1756bc1c69441ca739d88
adarsh <adarshjayasanker5@gmail.com>
2023-11-20 17:10:19 +0530

    added the admin template

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var ejs = require('ejs')
 
+
+var indexRouter = require('./routes/admin');
 var usersRouter = require('./routes/users');
-var adminRouter = require('./routes/admin');
+
 
 var app = express();
 
@@ -20,8 +22,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/admin', indexRouter);
 app.use('/', usersRouter);
-app.use('/users', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
